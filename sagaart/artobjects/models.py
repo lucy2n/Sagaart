9bd8a1e1cdd6508a05ef4b095051cdb38bc53059
Антон Арефин <r4zeel@yandex.ru>
2024-06-10 17:53:55 +0300

    add estimated file structure

---
+++ sagaart/artobjects/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+
+class Category(models.Model):
+    pass
+
+
+class Style(models.Model):
+    pass
+
+
+class Genre(models.Model):
+    pass
+
+
+class ArtObject(models.Model):
+    pass
+
+
+class ObjectAuthor(models.Model):
+    pass
