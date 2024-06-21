8bf51f8d1d7d9fcd75fe6a8fd76826bf22fca80b
Антон Арефин <r4zeel@yandex.ru>
2024-06-21 19:49:36 +0300

    add ci/cd pipeline and docker-compose-production

--- backend/sagaart/sagaart/settings.py
+++ backend/sagaart/sagaart/settings.py
@@ -57,9 +57,13 @@ TEMPLATES = [
 WSGI_APPLICATION = "sagaart.wsgi.application"
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT')
     }
 }
 
