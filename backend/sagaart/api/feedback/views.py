0ecf29d065f4a5c9a125bfe374c66872d6258115
Антон Арефин <r4zeel@yandex.ru>
2024-06-29 12:37:49 +0300

    change feedback send email

--- backend/sagaart/api/feedback/views.py
+++ backend/sagaart/api/feedback/views.py
@@ -1,7 +1,9 @@
-from django.core.mail import send_mail
+import django
+from django.core.mail import EmailMessage, get_connection
 from rest_framework import status, mixins, generics
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from django.conf import settings
 
 from .serializers import FeedbackSerializer
 from api.constants import FEEDBACK_EMAIL_ADRESS
@@ -11,15 +13,27 @@ class FeedbackView(generics.CreateAPIView):
     def post(self, request):
         serializer = FeedbackSerializer(data=request.data)
         if serializer.is_valid():
-            send_mail(
-                subject=f"Feedback from user: {self.request.user}",
-                message=(
+            with get_connection(
+                host=settings.EMAIL_HOST,
+                port=settings.EMAIL_PORT,
+                username=settings.EMAIL_HOST_USER,
+                password=settings.EMAIL_HOST_PASSWORD,
+                use_tls=settings.EMAIL_USE_TLS,
+            ) as connection:
+                subject = f"Feedback from user: {self.request.user}"
+                email_from = settings.EMAIL_HOST_USER
+                recipient_list = [settings.EMAIL_HOST_USER]
+                message = (
                     f"Message: {serializer.data['feedback_message']}, "
                     f"user name: {serializer.data['user_name']}, "
                     f"user phone: {serializer.data['user_phone']}"
-                ),
-                from_email=serializer.data["user_mail"],
-                recipient_list=[FEEDBACK_EMAIL_ADRESS],
-            )
+                )
+                EmailMessage(
+                    subject,
+                    message,
+                    email_from,
+                    recipient_list,
+                    connection=connection,
+                ).send()
             return Response(status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
--- backend/sagaart/sagaart/settings.py
+++ backend/sagaart/sagaart/settings.py
@@ -110,7 +110,7 @@ REST_FRAMEWORK = {
 LANGUAGE_CODE = "en-us"
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = 'smtp.mail.ru'
+EMAIL_HOST = "smtp.mail.ru"
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 EMAIL_USE_TLS = True
